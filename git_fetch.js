4e5dd7923799b5f1307d8990160005653eacd0d4
Matthew McCawley <matthew.w.mccawley@gmail.com>
2022-01-19 22:36:02 -0500

    Remove config download

--- git_fetch.js
+++ git_fetch.js
@@ -40,7 +40,6 @@ export async function main(ns) {
 
 async function fetchConfig(ns) {
 	try {
-		await getFileFromGH(ns, configFileName);
 		let json = ns.read(configFileName);
 		return JSON.parse(json);
 	}catch(e){
